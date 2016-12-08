351ddfca2b8700f994661fce7f9690a7a77778c4
Robert Rasmussen <bobbyras90@gmail.com>
2016-12-07 19:54:37 -0700

    Added linmath. Used the given file to create a rotating box of the ppm image.

--- ezview.c
+++ ezview.c
@@ -1,39 +1,99 @@
-#include <ctype.h>
+#define GLFW_DLL 1
+
+#define GL_GLEXT_PROTOTYPES
+#include <GLES2/gl2.h>
+#include <GLFW/glfw3.h>
+
+#include "linmath.h"
+
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <assert.h>
 
-typedef struct Pixel {
-	unsigned char r, g, b;
-} Pixel;
+typedef struct {
+  float Position[2];
+  float TexCoord[2];
+} Vertex;
 
-char convertFrom;
-char convertTo;
+Vertex vertexes[] = {
+  {{1, -1}, {0.99999, 0}},
+  {{1, 1},  {0.99999, 0.99999}},
+  {{-1, 1}, {0, 0.99999}},
+  {{-1, 1}, {0, 0.99999}},
+  {{-1, -1}, {0, 0}},
+  {{1, -1}, {0.99999, 0}}
+};
 
-int width, height;
+const GLubyte indices[] = {
+  0, 1, 2,
+  2, 3, 0
+};
 
-int maxColorValue;
+static const char* vertex_shader_text =
+"uniform mat4 MVP;\n"
+"attribute vec2 TexCoordIn;\n"
+"attribute vec2 vPos;\n"
+"varying vec2 TexCoordOut;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"    TexCoordOut = TexCoordIn;\n"
+"}\n";
 
-Pixel* pixmap;
-FILE* fh;
+static const char* fragment_shader_text =
+"varying lowp vec2 TexCoordOut;\n"
+"uniform sampler2D Texture;\n"
+"void main()\n"
+"{\n"
+"    gl_FragColor = texture2D(Texture, TexCoordOut);\n"
+"}\n";
 
-void getConversionType(char* arg) {
-	if (!strcmp(arg, "3") && !strcmp(arg, "6")) {
-		fprintf(stderr, "Error: Target needs to be 3 or 6.\n");
-		exit(1);
-	}
-	
-	convertTo = arg[0];
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
 }
 
-void getPPMFileType() {
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, 1);
+}
+
+void glCompileShaderOrDie(GLuint shader) {
+  GLint compiled;
+  glCompileShader(shader);
+  glGetShaderiv(shader,
+		GL_COMPILE_STATUS,
+		&compiled);
+  if (!compiled) {
+    GLint infoLen = 0;
+    glGetShaderiv(shader,
+		  GL_INFO_LOG_LENGTH,
+		  &infoLen);
+    char* info = malloc(infoLen+1);
+    GLint done;
+    glGetShaderInfoLog(shader, infoLen, &done, info);
+    printf("Unable to compile shader: %s\n", info);
+    exit(1);
+  }
+}
+
+int ppmFileType;
+int maxColorValue;
+
+int image_width;
+int image_height;
+
+unsigned char* image;
+
+void getPPMFileType(FILE* fh) {
 	char PPMFileType [4];
-	
+
 	if (fgets(PPMFileType, 4, fh) != NULL) {
 		if (!strcmp(PPMFileType, "P3\n")) {
-			convertFrom = '3';
+			ppmFileType = '3';
 		} else if (!strcmp(PPMFileType, "P6\n")) {
-			convertFrom = '6';
+			ppmFileType = '6';
 		} else {
 			fprintf(stderr, "Error: Not a PPM file. Incompatible file type.\n");
 			exit(1);
@@ -41,29 +101,29 @@ void getPPMFileType() {
 	}
 }
 
-void getWidthAndHeight() {
+void getWidthAndHeight(FILE* fh) {
 	char value[20];
-	
+
 	while ((value[0] = fgetc(fh)) == '#') {	// Line is a comment
 		while (fgetc(fh) != '\n');	// Ignore the line
 	}
-	
+
 	if (!isdigit(value[0])) {
 		fprintf(stderr, "Error: Image width is not valid.\n");
 		exit(1);
 	}
-	
+
 	int i = 1;
 	while ((value[i] = fgetc(fh)) != ' ') {
-		if (!isdigit(value[i])) { 
+		if (!isdigit(value[i])) {
 			fprintf(stderr, "Error: Image width is not valid.\n");
 			exit(1);
 		}
 		i++;
 	}
 	value[i] = '\0';
-	width = atoi(value);
-	
+	image_width = atoi(value);
+
 	i = 0;
 	while ((value[i] = fgetc(fh)) != '\n') {
 		if (!isdigit(value[i])) {
@@ -73,12 +133,12 @@ void getWidthAndHeight() {
 		i++;
 	}
 	value[i] = '\0';
-	height = atoi(value);
+	image_height = atoi(value);
 }
 
-void getMaxColorValue() {
+void getMaxColorValue(FILE* fh) {
 	char value[4];
-	
+
 	for (int i = 0; i < 4; i++) {
 		value[i] = fgetc(fh);
 		if (value[i] == '\n') {
@@ -89,9 +149,9 @@ void getMaxColorValue() {
 			exit(1);
 		}
 	}
-	
+
 	maxColorValue = atoi(value);
-	
+
 	if (maxColorValue > 255) {
 		fprintf(stderr, "Error: Not a PPM file. Max color value too high.\n");
 		exit(1);
@@ -100,10 +160,11 @@ void getMaxColorValue() {
 		exit(1);
 	}
 }
-void getP3Value(unsigned char* outValue) {
+
+void getP3Value(FILE* fh, unsigned char* outValue) {
 	unsigned char value[4];
 	int rgbValue;
-	
+
 	for (int i = 0; i < 4; i++) {
 		value[i] = fgetc(fh);
 		if (value[i] == '\n') {
@@ -114,88 +175,177 @@ void getP3Value(unsigned char* outValue) {
 			exit(1);
 		}
 	}
-	
+
 	rgbValue = atoi(value);
 	if (rgbValue > maxColorValue) {
 		fprintf(stderr, "Error: Color value exceeding max.\n");
 		exit(1);
 	}
-	
+
 	*outValue = rgbValue;
 }
-void parseP3() {
-	for (int i = 0; i < width * height; i++) {	
-		getP3Value(&pixmap[i].r);
-		getP3Value(&pixmap[i].g);
-		getP3Value(&pixmap[i].b);
+
+void parseP3(FILE* fh) {
+	for (int i = 0; i < image_width * image_height * 3; i += 3) {
+		getP3Value(fh, &image[i]);
+		getP3Value(fh, &image[i+1]);
+		getP3Value(fh, &image[i+2]);
 	}
 }
 
-void parseP6() {
-	fread(pixmap, sizeof(Pixel), width * height, fh);
+void parseP6(FILE* fh) {
+	fread(image, sizeof(unsigned char), image_width * image_height * 3, fh);
 }
 
-void loadPPM() {
-	getWidthAndHeight();
-	pixmap = malloc(sizeof(Pixel) * width * height);
-	getMaxColorValue();
-	if (convertFrom == '3') {
-		parseP3();
-	} else if (convertFrom == '6') {
-		parseP6();
-	}
-}
-
-void writeP3() {
-	fprintf(fh, "P3\n# Converted with Robert Rasmussen's ppmrw\n%d %d\n%d\n", width, height, maxColorValue);
-	for (int i = 0; i < width * height; i++) {
-		fprintf(fh, "%d\n%d\n%d\n", pixmap[i].r, pixmap[i].g, pixmap[i].b);
+void loadPPM(FILE* fh) {
+	getWidthAndHeight(fh);
+	image = malloc(sizeof(unsigned char) * image_width * image_height * 3);
+	getMaxColorValue(fh);
+	if (ppmFileType == '3') {
+		parseP3(fh);
+	} else if (ppmFileType == '6') {
+		parseP6(fh);
 	}
 }
 
-void writeP6() {
-	fprintf(fh, "P6\n# Converted with Robert Rasmussen's ppmrw\n%d %d\n%d\n", width, height, maxColorValue);
-	fwrite(pixmap, sizeof(Pixel), width*height, fh);
+int main(int argc, char* argv[])
+{
+  FILE* fh;
+
+  fh = fopen(argv[1], "rb");
+  if (fh == NULL) {
+    fprintf(stderr, "Error: Input file not found.\n");
+    return 1;
+  }
+
+  getPPMFileType(fh);
+
+  loadPPM(fh);
+  fclose(fh);
+
+  printf("%d", image[0]);
+
+    GLFWwindow* window;
+    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+    GLint mvp_location, vpos_location, vcol_location;
+
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    glfwDefaultWindowHints();
+    glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    window = glfwCreateWindow(640, 480, "ezview", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwSetKeyCallback(window, key_callback);
+
+    glfwMakeContextCurrent(window);
+    // gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+    glfwSwapInterval(1);
+
+    // NOTE: OpenGL error checks have been omitted for brevity
+
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexes), vertexes, GL_STATIC_DRAW);
+
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+    glCompileShaderOrDie(vertex_shader);
+
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+    glCompileShaderOrDie(fragment_shader);
+
+    program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+    // more error checking! glLinkProgramOrDie!
+
+    mvp_location = glGetUniformLocation(program, "MVP");
+    assert(mvp_location != -1);
+
+    vpos_location = glGetAttribLocation(program, "vPos");
+    assert(vpos_location != -1);
+
+    GLint texcoord_location = glGetAttribLocation(program, "TexCoordIn");
+    assert(texcoord_location != -1);
+
+    GLint tex_location = glGetUniformLocation(program, "Texture");
+    assert(tex_location != -1);
+
+    glEnableVertexAttribArray(vpos_location);
+    glVertexAttribPointer(vpos_location,
+			  2,
+			  GL_FLOAT,
+			  GL_FALSE,
+                          sizeof(Vertex),
+			  (void*) 0);
+
+    glEnableVertexAttribArray(texcoord_location);
+    glVertexAttribPointer(texcoord_location,
+			  2,
+			  GL_FLOAT,
+			  GL_FALSE,
+                          sizeof(Vertex),
+			  (void*) (sizeof(float) * 2));
+
+    GLuint texID;
+    glGenTextures(1, &texID);
+    glBindTexture(GL_TEXTURE_2D, texID);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image_width, image_height, 0, GL_RGB,
+		 GL_UNSIGNED_BYTE, image);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texID);
+    glUniform1i(tex_location, 0);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float ratio;
+        int width, height;
+        mat4x4 m, p, mvp;
+
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float) height;
+
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        mat4x4_identity(m);
+        mat4x4_rotate_Z(m, m, (float) glfwGetTime());
+        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        mat4x4_mul(mvp, p, m);
+
+        glUseProgram(program);
+        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 3, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    free(image);
+    exit(EXIT_SUCCESS);
 }
 
-int main(int argc, char* argv[]) {
-	if (argc != 4) {
-		fprintf(stderr, "Usage: ppmrw target input output.\n");
-		return 1;
-	}
-	
-	getConversionType(argv[1]);
-	
-	fh = fopen(argv[2], "rb");
-	if (fh == NULL) {
-		fprintf(stderr, "Error: Input file not found.\n");
-		return 1;
-	}
-	
-	getPPMFileType();
-	
-	loadPPM();
-	
-	fclose(fh);
-	
-	if (convertTo == '3') {
-		fh = fopen(argv[3], "w");
-	} else if (convertTo == '6') {
-		fh = fopen(argv[3], "wb");
-	}
-	if (fh == NULL) {
-		fprintf(stderr, "Error: Output file not found.\n");
-		return 1;
-	}
-	
-	if (convertTo == '3') {
-		writeP3();
-	} else if (convertTo = '6') {
-		writeP6();
-	}
-	
-	fclose(fh);
-	free(pixmap);
-	
-	return 0;
-}
+//! [code]
